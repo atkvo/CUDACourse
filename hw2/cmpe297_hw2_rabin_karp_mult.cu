9b9264cae1a362e5abe215d5c973874bee8f1e7d
voandrew <voandrew.khoa@gmail.com>
2016-09-26 23:34:47 -0700

    working multiple pattern search

--- hw2/cmpe297_hw2_rabin_karp_mult.cu
+++ hw2/cmpe297_hw2_rabin_karp_mult.cu
@@ -8,39 +8,38 @@
 
 /*ADD CODE HERE: Implement the parallel version of the sequential Rabin-Karp*/
 __global__ void 
-findIfExistsCu(char* input, int input_length, char* pattern, int pattern_length, int patHash, int* result, unsigned long long* runtime)
+// findIfExistsCu(char* input, int input_length, char* pattern, int pattern_length, int patHash, int* result, unsigned long long* runtime)
+findIfExistsCu(char* input, int input_length, char* pattern, int numberOfPatterns, int* patternLengths, int *startstops, int* patHash, int* result, unsigned long long* runtime)
 { 
     unsigned long long start_time = clock64();
+    int patIndex = blockIdx.x;   // use this to index the pattern 
     int tid = threadIdx.x;  // tid is where the search index will start
     int input_hash = 0;
-    for(int k = tid; k < (pattern_length + tid); k++) {
-        input_hash = (input_hash * 256 + input[k]) % 997;
-    }
 
-    // unable to call "host" functions in gpu (memcmp)
-    // if(input_hash == patHash && memcmp(input + tid*sizeof(char), pattern, pattern_length) == 0) {
-
-    if(input_hash == patHash) {
-        printf("+++ match at: %i\n", tid);
-        result[tid] = 1;
+    if (tid < (input_length - patternLengths[patIndex])) {
+        for(int k = tid; k < (patternLengths[patIndex] + tid); k++) {
+            input_hash = (input_hash * 256 + input[k]) % 997;
+        }
+        if(input_hash == patHash[patIndex]) {
+            printf("+++ match @ %i\n", patIndex);
+            result[patIndex] |= 1;
+        }
+        else { 
+            result[patIndex] |= 0; 
+        } 
+        unsigned long long stop_time = clock64();
+        runtime[tid] = (unsigned long long)(stop_time - start_time);
     }
-    else { 
-        result[tid] = 0; 
-    } 
-    unsigned long long stop_time = clock64();
-    runtime[tid] = (unsigned long long)(stop_time - start_time);
 }
 
 int main()
 {
     // host variables
     char input[] = "HEABAL AND SOME MORE CHARACTERS";    /*Sample Input*/
-    char pattern[] = "AB";      /*Sample Pattern*/
-    int patHash = 0;            /*hash for the pattern*/
+    int* patHash;               /*hash for the pattern*/
     int* result;                /*Result array*/
     unsigned long long* runtime;               /*Exection cycles*/
-    int pattern_length = 2;     /*Pattern Length*/
-    int input_length = 32;       /*Input Length*/
+    int input_length = 31;       /*Input Length*/
 
     const int numberOfPatterns = 4;
     char* patterns2d[] = {
@@ -53,6 +52,7 @@ int main()
         6, 8,
         9, 12
     };
+    patHash = (int*)malloc(sizeof(int)*numberOfPatterns);
 
     // flatten the 2d character array
     int totalLengths = 0;
@@ -64,11 +64,11 @@ int main()
     for(int i = 0; i < numberOfPatterns; i++) {
         for(int j = 0; j < lengths[i]; j++) {
             patternsFlat[startstops[i*2] + j] = patterns2d[i][j];
-            printf("%c", patterns2d[i][j]);
+            // printf("%c", patterns2d[i][j]);
         }
         printf("\n");
     }
-    printf("Done flattening 2d array\n");
+    // printf("Done flattening 2d array\n");
     // done flattening 2d array into patternsFlat
     
 
@@ -78,18 +78,21 @@ int main()
     int* d_result;
     int* d_startstops;
     int* d_patternLengths;
+    int* d_patHash;
     unsigned long long* d_runtime;
 
     // measure the execution time by using clock() api in the kernel as we did in Lab3
-    int runtime_size = (input_length-pattern_length)*sizeof(unsigned long long);
-    result = (int*)malloc((input_length-pattern_length)*sizeof(int));
+    int runtime_size = (input_length*numberOfPatterns)*sizeof(unsigned long long);
+    result = (int*)malloc((input_length*numberOfPatterns)*sizeof(int));
     runtime = (unsigned long long *) malloc(runtime_size);
     memset(runtime, 0, runtime_size);
     
     /*Calculate the hash of the pattern*/
-    for (int i = 0; i < pattern_length; i++)
-    {
-        patHash = (patHash * 256 + pattern[i]) % 997;
+    for (int i = 0; i < numberOfPatterns; i++) {
+        for (int j = 0; j < lengths[i]; j++)
+        {
+            patHash[i] = (patHash[i] * 256 + patterns2d[i][j]) % 997;
+        }
     }
 
     /*ADD CODE HERE: Allocate memory on the GPU and copy or set the appropriate values from the HOST*/
@@ -100,9 +103,27 @@ int main()
         exit(EXIT_FAILURE);
     }
     
-    err = cudaMalloc((void**)&d_pattern, pattern_length*sizeof(char));
+    err = cudaMalloc((void**)&d_pattern, totalLengths*sizeof(char));
     if (err != cudaSuccess) {
-        fprintf(stderr, "Failed to allocate device input string. (ERR: %s)\n", cudaGetErrorString(err));
+        fprintf(stderr, "Failed to allocate device pattern 1d string. (ERR: %s)\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMalloc((void**)&d_patternLengths, numberOfPatterns*sizeof(int));
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to allocate device pattern len array. (ERR: %s)\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMalloc((void**)&d_startstops, numberOfPatterns*2*sizeof(int));
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to allocate device pattern start stops array. (ERR: %s)\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMalloc((void**)&d_patHash, numberOfPatterns*sizeof(int));
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to allocate device pattern hash array. (ERR: %s)\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
@@ -125,17 +146,37 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    err = cudaMemcpy(d_pattern, pattern, pattern_length*sizeof(char), cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_pattern, patternsFlat, totalLengths*sizeof(char), cudaMemcpyHostToDevice);
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to copy pattern from host to device. (ERR: %s)\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
+    err = cudaMemcpy(d_patternLengths, lengths, numberOfPatterns*sizeof(int), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to copy pattern lengths from host to device. (ERR: %s)\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(d_startstops, startstops, numberOfPatterns*2*sizeof(int), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to copy pattern start stops from host to device. (ERR: %s)\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(d_patHash, patHash, numberOfPatterns*sizeof(int), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to copy pattern hashes from host to device. (ERR: %s)\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
     /*ADD CODE HERE: Launch the kernel and pass the arguments*/
-    int blocksPerGrid = 1;
-    int threadsPerBlock = input_length - pattern_length;
+    int blocksPerGrid = numberOfPatterns;
+    // int threadsPerBlock = input_length - pattern_length;
+    int threadsPerBlock = input_length; // use the maximum amount of threads needed per pattern just in case
     printf("Running kernel functions blocks: %i and threads: %i\n", blocksPerGrid, threadsPerBlock); 
-    findIfExistsCu<<<blocksPerGrid, threadsPerBlock>>>(d_input, input_length, d_pattern, pattern_length, patHash, d_result, d_runtime);
+    // findIfExistsCu<<<blocksPerGrid, threadsPerBlock>>>(d_input, input_length, d_pattern, pattern_length, patHash, d_result, d_runtime);
+    findIfExistsCu<<<blocksPerGrid, threadsPerBlock>>>(d_input, input_length, d_pattern, numberOfPatterns, d_patternLengths, d_startstops, d_patHash, d_result, d_runtime);
         
     err = cudaGetLastError();
     if (err != cudaSuccess) {
@@ -151,7 +192,7 @@ int main()
     
     /*RUN TIME calculation*/
     unsigned long long elapsed_time = 0;
-    for(int i = 0; i < input_length-pattern_length; i++)
+    for(int i = 0; i < input_length*numberOfPatterns; i++)
         if(elapsed_time < runtime[i])
             elapsed_time = runtime[i];
 
@@ -170,9 +211,13 @@ int main()
     printf("\nRESULTS\n"); 
     printf("-----------\n"); 
     printf("Input string = %s\n", input); 
-    printf("Pattern      = %s\n", pattern); 
-    for (int i = 0; i < input_length; i++) { 
-        printf("Pos: %i Result: %i\n", i, result[i]);
+    for (int i = 0; i < numberOfPatterns; i++) { 
+        if(result[i] == 1) {
+            printf("Pattern:  %s \twas found.\n", patterns2d[i]);
+        }
+        else {
+            printf("Pattern:  %s \twas NOT found.\n", patterns2d[i]);
+        }
     }
 
     cudaFree(d_input);
